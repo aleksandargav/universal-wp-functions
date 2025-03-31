a4db4b6b3039f669f1f44ff7c50168c4775fb89d
aleksandargav <aleksanda@massvision.net>
2025-03-31 12:58:26 +0200

    added translate

--- functions.php
+++ functions.php
@@ -4,9 +4,11 @@
  * Učitava sve prilagođene WordPress funkcije
  */
 
+// Path to the folder with functions
 // Putanja do foldera sa funkcijama
 $functions_dir = __DIR__ . '/functions';
 
+// Load all PHP files from the 'functions' folder
 // Učitaj sve PHP fajlove iz foldera 'functions'
 foreach (glob($functions_dir . '/*.php') as $file) {
     require_once $file;
